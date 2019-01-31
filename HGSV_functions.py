5abac182e61154fdc993f204b345eda6bf972575
cmlnodzak <cmlnodzak@users.noreply.github.com>
2019-01-31 11:17:24 -0500

    added pybedtools

--- HGSV_functions.py
+++ HGSV_functions.py
@@ -24,8 +24,9 @@ pref = '.'.join(infile.split('/')[-1].split('.')[0].split("_")[0:2])
 outfy = pref+'.'+svtype+'_integrate.bed'
 
 
-df = pd.DataFrame()
+col_names =  ['#chrom', 'start', 'stop', 'REF', 'ALT', 'QUAL', 'FILT', 'sample', 'GT']
 
+df  = pd.DataFrame(columns = col_names)
 # Extract the relevant information to bed file for safe keeping, and a pandas 
 # DataFrame and generate a subsequent coordinate intersections 
 # with genome annotations.
@@ -55,10 +56,35 @@ class VCFparser:
                                     df = df.append(call)
                                     outfile.write(call+'\n')
 
+# extract heterozygous deletions and insertions, group by samples.
+df = df.loc[df['GT'] == '0/1']
+
+df = df.groupby('sample')
+samp512 = df.get_group('HG00512').drop_duplicates()
+samp513 = df.get_group('HG00513').drop_duplicates()
+samp514 = df.get_group('HG00514').drop_duplicates()
+samp731 = df.get_group('HG00731').drop_duplicates()
+samp732 = df.get_group('HG00732').drop_duplicates()
+samp733 = df.get_group('HG00733').drop_duplicates()
+samp238 = df.get_group('NA19238').drop_duplicates()
+samp239 = df.get_group('NA19239').drop_duplicates()
+samp240 = df.get_group('NA19240').drop_duplicates()
+
+HG00512 = BedTool.from_dataframe(samp512)
+HG00513 = BedTool.from_dataframe(samp513)
+HG00514 = BedTool.from_dataframe(samp514)
+HG00731 = BedTool.from_dataframe(samp731)
+HG00732 = BedTool.from_dataframe(samp732)
+HG00733 = BedTool.from_dataframe(samp733)
+NA19238 = BedTool.from_dataframe(samp238)
+NA19239 = BedTool.from_dataframe(samp239)
+NA19240 = BedTool.from_dataframe(samp240)
 
 
 
-x = BedTool.from_dataframe(df)
+# x = BedTool.from_dataframe(df)
+
+
 
 
 
