80bde3a1a248246a3ec14962a72eef10606ea1f6
cmlnodzak <cmlnodzak@users.noreply.github.com>
2019-02-01 12:35:40 -0500

    added optional arguments to methods and documentation

--- helperFunctions.py
+++ helperFunctions.py
@@ -20,15 +20,58 @@ def orderOfMag(val):
 #gtf ='gencode.v25.annotation.gtf'
 
 # extract pandas dataframe from a GTF for a particular feature of interest.
-def gencodeReader(gtf, feat):
+
+def _attributeExtractor(attr_df):
+    '''
+    =================================================================+========
+    Private method used by gtfReader() method to extract attributes 
+    from a Pandas DataFrame. Returns a 12 column DataFrame with original 
+    'attributes' column dropped.
+    ==========================================================================
+    '''
+    attr_df['gene_id'] = attr_df['attributes'].map(lambda x: re.search(r'gene_id "ENSG\d+.\d+', x)[0].split()[1].strip("\""))    
+    attr_df['gene_type'] = attr_df['attributes'].map(lambda x: re.search(r'gene_type\s"\w+',x)[0].split()[1].strip("\""))
+    attr_df['gene_name'] = attr_df['attributes'].map(lambda x: re.search(r'gene_name\s"\w+',x)[0].split()[1].strip("\""))
+    attr_df['gene_status'] = attr_df['attributes'].map(lambda x: re.search(r'gene_status\s"\w+',x)[0].split()[1].strip("\""))
+    attr_df = attr_df.drop('attributes',axis=1)
+    return attr_df
+
+def gtfReader(gtf, feat, group = None, chrom = None):
+    '''
+    ==========================================================================
+    gtfReader: a function to parse gtf files into clean pandas Dataframes.
+    ==========================================================================
+    parameters:
+    ==========================================================================
+        -- gtf: a gene transfer format file. 
+            (see here ensembl.org/info/website/upload/gff.html for more.)
+        -- feat: a feature of interest, may be one of 'gene', 'transcript', 
+                'exon'.
+        -- group: optional, the gene_type that the feature maps to. may be 
+            'protein_coding, lncRA', etc. 
+            For full list run gtfReader() then df.gene_type.drop_duplicates().
+        -- chrom: optional, the name of the chromosome found in the '
+                seqname' field.
+    ==========================================================================
+    output:
+    ==========================================================================
+        Returns a Pandas DataFrame with column names listed below.
+        column names = ['seqname', 'source', 'feature', 'start', 'end', 
+                            'score', 'strand', 'frame', 'gene_id', 
+                            'gene_type', 'gene_name', 'gene_status']
+    ==========================================================================
+    '''
     anno_df = BedTool(gtf).to_dataframe()
     anno_df = anno_df[anno_df.feature == feat]
-    anno_df['gene_id'] = anno_df['attributes'].map(lambda x: re.search(r'gene_id "ENSG\d+.\d+', x)[0].split()[1].strip("\""))    
-    anno_df['gene_type'] = anno_df['attributes'].map(lambda x: re.search(r'gene_type\s"\w+',x)[0].split()[1].strip("\""))
-    anno_df['gene_name'] = anno_df['attributes'].map(lambda x: re.search(r'gene_name\s"\w+',x)[0].split()[1].strip("\""))
-    anno_df['gene_status'] = anno_df['attributes'].map(lambda x: re.search(r'gene_status\s"\w+',x)[0].split()[1].strip("\""))
-    anno_df = anno_df.drop('attributes',axis=1)
+    anno_df = _attributeExtractor(anno_df)
+    if group == 'lncRNA':
+        group = ['lincRNA', 'antisense', 'sense_overlapping', 'macro_lncRNA', 'sense_intronic', 'bidirectional_promoter_lncRNA' ]
+        anno_df = anno_df[anno_df['gene_type'].isin(group)].reset_index(drop=True)   
+    elif group != None:
+        anno_df = anno_df[anno_df.gene_type == group].reset_index(drop=True)
+    if chrom != None:
+        anno_df[anno_df.seqname == chrom].reset_index(drop=True)
     return anno_df
 
-attr = {'gene_id': None, 'gene_type': None , 
-        'gene_name': None, 'gene_status' : None}
+
+
